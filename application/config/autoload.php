12aea85d40405493f761171a04fb9d1544ba01c6
RendyPriyadiSuhendi <rendysmity12@gmail.com>
2021-11-19 18:51:38 +0700

    PERTEMUAN 10

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $autoload['packages'] = array();
 $autoload['libraries'] = array('form_validation', 'database', 'session');
 $autoload['drivers'] = array();
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url', 'perpus_helper');
 $autoload['config'] = array();
 $autoload['language'] = array();
 $autoload['model'] = array('ModelUser', 'ModelBuku');
